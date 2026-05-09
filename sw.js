fe79ba9da77a058a9701005319bbb8a96cd99037
m66751225-arch <m66751225@gmail.com>
2026-05-10 01:52:32 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,75 @@
+const CACHE_NAME = 'mobywatel-cache-v2';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './mo_ui_premium.css',
+    './app.css',
+    './coi_common_ui_ic_mobywatel_logo.svg',
+    './flaga.gif',
+    './godlo.gif'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+                return cache.addAll(ASSETS_TO_CACHE);
+            })
+    );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request).then(
+                    (response) => {
+                        // Check if we received a valid response
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            // For external assets like Google Fonts, we might want to cache them anyway
+                            if (event.request.url.includes('googleapis') || event.request.url.includes('gstatic')) {
+                                const responseToCache = response.clone();
+                                caches.open(CACHE_NAME)
+                                    .then((cache) => {
+                                        cache.put(event.request, responseToCache);
+                                    });
+                                return response;
+                            }
+                            return response;
+                        }
+
+                        const responseToCache = response.clone();
+                        caches.open(CACHE_NAME)
+                            .then((cache) => {
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    }
+                );
+            })
+    );
+});
+
+// Update a service worker
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
